0ce7cb97abb67f298bb26a6f303faeedd08e22c3
Shivamrathi001 <106222099+Shivamrathi001@users.noreply.github.com>
2022-10-31 12:40:08 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
-sudo docker run -d -p 27017:27017 --network mongo-network --name mongodb mongo
-sudo docker run -d -p 80:80 --network mongo-network 548893471177.dkr.ecr.us-east-1.amazonaws.com/blogpost:latest
-
+# sudo docker run -d -p 27017:27017 --network mongo-network --name mongodb mongo
+# sudo docker run -d -p 80:80 --network mongo-network 548893471177.dkr.ecr.us-east-1.amazonaws.com/blogpost:latest
+sudo docker compose down
+sudo docker compose up
