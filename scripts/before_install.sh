d352a381f2b055f4340f9967c5935c3b56fd8194
Shivamrathi001 <106222099+Shivamrathi001@users.noreply.github.com>
2022-10-31 12:15:29 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,5 @@
 sudo aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 548893471177.dkr.ecr.us-east-1.amazonaws.com
 sudo docker pull 548893471177.dkr.ecr.us-east-1.amazonaws.com/blogpost:latest
+sudo docker rm -f $(sudo docker ps -a -q)
+sudo docker network rm  mongo-network
 sudo docker network create -d bridge mongo-network
