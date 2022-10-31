4f1d9887ae687cff02fd7897ca8de6318c9e835e
Shivamrathi001 <106222099+Shivamrathi001@users.noreply.github.com>
2022-10-31 11:15:16 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,2 @@
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 548893471177.dkr.ecr.us-east-1.amazonaws.com
-sudo docker pull 548893471177.dkr.ecr.us-east-1.amazonaws.com/blogpost:latest
-sudo docker network create -d bridge mongo-network
 sudo docker run -d -p 27017:27017 --network mongo-network --name mongodb mongo
 sudo docker run -d -p 80:80 --network mongo-network 548893471177.dkr.ecr.us-east-1.amazonaws.com/blogpost:latest
